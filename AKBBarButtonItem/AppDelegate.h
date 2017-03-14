db2fa6322c9e012f2547375d5fa84e5c2ec274a7
v <524684997@qq.com>
2017-03-14 14:15:10 +0800

    第一次提交

---
+++ AKBBarButtonItem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AKBBarButtonItem
+//
+//  Created by v on 17/3/14.
+//  Copyright © 2017年 v. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
