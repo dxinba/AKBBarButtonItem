7109418e3b79670a838d733770c2c15c556637a0
zxb <1030075254@qq.com>
2017-04-14 09:20:01 +0800

    README

--- AKBBarButtonItem/ViewController.m
+++ AKBBarButtonItem/ViewController.m
@@ -11,10 +11,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    
-    self.akb_title(@"asd").akb_item([[UIBarButtonItem alloc] initWithTitle:@"asd" style:UIBarButtonItemStylePlain target:self action:@selector(barBtnItemClick)]).akb_onLeft();
-    
-    self.akb_image(@"star_h").akb_item([[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"star_h"] style:UIBarButtonItemStylePlain target:self action:@selector(barBtnItemClick)]).akb_onRight();
+    self.akb_imageAndTitle(@"star_h",@"asd").akb_onLeft();
+//    self.akb_title(@"asd").akb_item([[UIBarButtonItem alloc] initWithTitle:@"asd" style:UIBarButtonItemStylePlain target:self action:@selector(barBtnItemClick)]).akb_onLeft();
+//    
+//    self.akb_image(@"star_h").akb_item([[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"star_h"] style:UIBarButtonItemStylePlain target:self action:@selector(barBtnItemClick)]).akb_onRight();
 }
 
 - (void)barBtnItemClick {
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # AKBBarButtonItem
-用 `UIViewController` 类别来 封装 `UIBarButtonItem` 的添加或移除方法
+
+用 `UIViewController` 类别来封装 `UIBarButtonItem` 的添加和移除方法
+
+## 使用方法
+* 添加纯文本按钮
+	* 添加一个   
+	
+	``` objc
+	self.akb_title(@"按钮A").akb_onLeft();
+	```
+	
+	* 添加多个   
+	
+	``` objc
+	self.akb_title(@[@"按钮A", @"按钮B",...]).akb_onLeft();
+	```
+	
+* 添加纯图片按钮
+	* 添加一个   
+	
+	``` objc
+	self.akb_image(@"图片名A").akb_onRight();
+	```
+	
+	* 添加多个   
+	
+	``` objc
+	self.akb_image(@[@"图片名A", @"图片名B",...]).akb_onRight();
+	```
+	
+* 添加图片+标题按钮
+
+	``` objc
+	self.akb_image(@"图片名A").akb_onRight();
+	```	
+	
+## 点击事件
+> 在controller中重写👇的方法即可，` tag `都是不一样的。
+
+``` objc
+- (void)akb_barButtonItemClick:(UIButton *)sender {
+    NSLog(@"%zi",sender.tag);
+}
+```
+
+* 还有更多对按钮的操作，请前往源代码查看。
